7b4d43447c38759de9a4c9126e842f2f385acb25
denys <denamyte@gmail.com>
2022-11-29 08:23:30 +0200

    2022-11-23: Stage 1/4: finished

--- Duplicate File Handler/task/handler.py
+++ Duplicate File Handler/task/handler.py
@@ -1,1 +1,16 @@
-# write your code here
+import sys
+from os import walk, path
+
+
+def main():
+    if len(sys.argv) != 2:
+        print('Directory is not specified')
+        return
+    folder = sys.argv[1]
+    for root, dirs, files in walk(folder):
+        for f_name in files:
+            print(path.join(root, f_name))
+
+
+if __name__ == '__main__':
+    main()
